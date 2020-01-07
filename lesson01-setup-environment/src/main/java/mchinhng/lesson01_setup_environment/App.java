5cd69a5ad3c5b9673e2348d26f3be5a482bb5a88
mchinhng <nguyentmchinh.pd@gmail.com>
2020-01-07 21:18:05 +0700

    lesson01-code-07.01.2020

---
+++ lesson01-setup-environment/src/main/java/mchinhng/lesson01_setup_environment/App.java
@@ -0,0 +1,11 @@
+package mchinhng.lesson01_setup_environment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
